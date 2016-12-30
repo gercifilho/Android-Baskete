fe4ad9f1e8ac24f6a88c15726d6932afd107a0e6
DESKTOP-3ESP4L4\Poço Martins <gerci.filho@gmail.com>
2016-12-30 18:56:57 -0200

    melhoria no layout

--- app/src/main/java/br/com/pocomartins/baskete/MainActivity.java
+++ app/src/main/java/br/com/pocomartins/baskete/MainActivity.java
@@ -7,7 +7,8 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private int pontuacaoTimeA =0;
+    private int pontuacaoTimeA = 0;
+    private int pontuacaoTimeB = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,7 +39,37 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        Button botaoTresPontosTimeB = (Button) findViewById(R.id.tresPontosB);
+        botaoTresPontosTimeB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addTresPontosTimeB();
+            }
+        });
 
+        Button botaodoisPontosTimeB = (Button) findViewById(R.id.doisPontosB);
+        botaodoisPontosTimeB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addDoisPontosTimeB();
+            }
+        });
+
+        Button tiroslivresB = (Button) findViewById(R.id.tiroslivresB);
+        tiroslivresB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addUmPontosTimeB();
+            }
+        });
+
+        Button reinicarPartida = (Button) findViewById(R.id.reiniciar);
+        reinicarPartida.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                reiniciarPartida();
+            }
+        });
 
 
     }
@@ -46,6 +77,11 @@ public class MainActivity extends AppCompatActivity {
     public void mostraPlacarTimeA(int pontuacao) {
         TextView placarTimeA = (TextView) findViewById(R.id.placaTimeA);
         placarTimeA.setText(String.valueOf(pontuacao));
+    }
+
+    public void mostraPlacarTimeB(int pontuacao) {
+        TextView placarTimeB = (TextView) findViewById(R.id.placaTimeB);
+        placarTimeB.setText(String.valueOf(pontuacao));
     }
 
     public void addTresPontosTimeA() {
@@ -61,5 +97,30 @@ public class MainActivity extends AppCompatActivity {
     public void addUmPontosTimeA() {
         pontuacaoTimeA = pontuacaoTimeA + 1;
         mostraPlacarTimeA(pontuacaoTimeA);
+    }
+
+    public void addTresPontosTimeB() {
+        pontuacaoTimeB = pontuacaoTimeB + 3;
+        mostraPlacarTimeB(pontuacaoTimeB);
+    }
+
+    public void addDoisPontosTimeB() {
+        pontuacaoTimeB = pontuacaoTimeB + 2;
+        mostraPlacarTimeB(pontuacaoTimeB);
+    }
+
+    public void addUmPontosTimeB() {
+        pontuacaoTimeB = pontuacaoTimeB + 1;
+        mostraPlacarTimeB(pontuacaoTimeB);
+    }
+
+    public void reiniciarPartida(){
+        pontuacaoTimeA = 0;
+        pontuacaoTimeB = 0;
+
+        mostraPlacarTimeA(pontuacaoTimeA);
+        mostraPlacarTimeB(pontuacaoTimeB);
+
+
     }
 }
